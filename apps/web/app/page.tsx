335106ed166f718c15486d0354117fc0991f8d96
NTA1210 <162094232+NTA1210@users.noreply.github.com>
2026-08-17 14:10:34 +0700

    feat(web): mount global editor toolbar and shortcuts

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,13 @@
 import EditorShell from '../components/EditorShell';
+import KeyboardShortcuts from '../components/KeyboardShortcuts';
 import SnapViewportHud from '../components/SnapViewportHud';
+import WorkspaceToolbar from '../components/WorkspaceToolbar';
 
 export default function Home(){
   return <>
     <EditorShell/>
+    <WorkspaceToolbar/>
     <SnapViewportHud/>
+    <KeyboardShortcuts/>
   </>;
 }
